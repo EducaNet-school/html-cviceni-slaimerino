fa90d283d02633d458d31e8b28d95b3bd71e6a11
Vojtěch Janovský <vojtech.janovsky@educanet.cz>
2023-02-15 11:35:31 +0100

    Nakupni kosik ukol

--- Java testy/public class StringPrinter {.java
+++
@@ -1,8 +0,0 @@
-public class StringPrinter {
-    public static void main(String[] args) {
-        String input = "Hello World!";
-        for (int i = 1; i <= input.length(); i++) {
-            System.out.println(input.substring(0, i));
-        }
-    }
-}
